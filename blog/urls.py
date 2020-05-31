4683ec580251e70d392aaba17e22afdd4c6f05d3
Sergi Orra <sergiorra17@gmail.com>
2020-05-31 17:36:13 +0200

    blog detail

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_blogs, name='all_blogs')
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Blog
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'blog': blog_id})
